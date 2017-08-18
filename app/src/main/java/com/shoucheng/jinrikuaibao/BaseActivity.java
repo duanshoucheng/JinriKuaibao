db30c06da11bdf9fbd38eaa390edbdc2a20120d8
souceng <dsc428@126.com>
2017-08-18 11:18:41 +0800

    Initial commit

---
+++ app/src/main/java/com/shoucheng/jinrikuaibao/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.shoucheng.jinrikuaibao;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 17-8-18.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/shoucheng/jinrikuaibao/MainActivity.java
@@ -0,0 +1,27 @@
+package com.shoucheng.jinrikuaibao;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.appName)
+    TextView appName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.appName)
+    public void onClick() {
+        startActivity(new Intent(this, SettingActivity.class));
+    }
+}
